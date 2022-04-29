eb58612abc2626cd9a061b050c62029953b10916
Marco Belo <marco.barone.belo@gmail.com>
2022-04-28 22:52:30 -0300

    feat: new adapters/web_server and cli http command

---
+++ adapters/web/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"github.com/codegangsta/negroni"
+	_ "github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	_ "github.com/gorilla/mux"
+	"github.com/marcobelo/arch_hexagonal_go/adapters/web/handler"
+	"github.com/marcobelo/arch_hexagonal_go/application"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+	r := mux.NewRouter()
+	n := negroni.New(negroni.NewLogger())
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
